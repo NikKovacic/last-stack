7a5a0299cd4c98bca2b5cc320f31d389bdaf9c41
nikkov <nik.kovacic10@gmail.com>
2024-09-30 20:01:28 +0200

    Add 3rd Party JavaScript Packages

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,9 @@
  */
 import './styles/app.css';
 import alienGreeting from './lib/alien-greetings.js';
+import JSConfetti from 'js-confetti';
 
+const jsConfetti = new JSConfetti();
+jsConfetti.addConfetti();
 alienGreeting('Give us all your candy!', false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
